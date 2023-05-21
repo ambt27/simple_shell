ef3b0015d1ca54a70f5258409f14e7bd7e4111b0
Andre Mbargou Tine <ambt27@gmail.com>
2023-05-21 07:13:11 -0700

    Commit 2

---
+++ passingbetty.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - function that prints something
+ * Return: 0 for success
+ */
+int main(void)
+{
+	printf("Hello Alx, \nthis is @Andre Mbargou Tine@\n");
+	return (0);
+}
